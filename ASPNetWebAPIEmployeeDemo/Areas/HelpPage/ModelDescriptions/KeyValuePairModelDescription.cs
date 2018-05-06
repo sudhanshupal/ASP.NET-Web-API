49ff78a56fe38877665caef0c9aa746f249104b5
sudhanshu <spal@dminc.com>
2018-05-06 17:59:41 +0530

    Changes

---
+++ ASPNetWebAPIEmployeeDemo/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ASPNetWebAPIEmployeeDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ ASPNetWebAPIEmployeeDemo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ASPNetWebAPIEmployeeDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
