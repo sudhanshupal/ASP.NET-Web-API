4dd041981caece937cd710ba8996b9e528aa858f
sudhanshu <spal@dminc.com>
2018-05-15 16:12:21 +0530

    All Changes

---
+++ ASPNetWebAPIEmployeeDemo/Controllers/EventController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Test.Repository.Concrete;
+using Test.Repository.Factory;
+
+namespace ASPNetWebAPIEmployeeDemo.Controllers
+{
+    public class EventController : ApiController
+    {
+        IEvents _repository;
+       public EventController(IEvents repository)
+       {
+            _repository = repository;
+       }
+       public void Post()
+        {
+
+        }
+    }
+}
---
+++ Test.DTO/EventDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.DTO
+{
+    class EventDetail
+    {
+        public int EventId { get; set; }
+        public string EventCode { get; set; }
+        public string EventName { get; set; }
+        public System.DateTime EventDate { get; set; }
+        public string DistanceCategories { get; set; }
+        public string EventCity { get; set; }
+        public string EventState { get; set; }
+        public string EventCountry { get; set; }
+        public string EventDetails { get; set; }
+        public string EventLocation { get; set; }
+        public decimal RegistrationFees { get; set; }
+        public string RegistrationLink { get; set; }
+    }
+}
